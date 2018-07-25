dcbd026cb3ecce2d80b29bf2645b3e69be5118c6
KMonahan <kevin.monahan@hotmail.com>
2018-07-25 10:03:03 -0500

    got the survey outputting

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
-var friends = require("app/data/friends")
+// var friends = require("app/data/friends")
 var app = express();
 
 var PORT = 3000;
@@ -9,13 +9,16 @@ var PORT = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json())
 
-var questions = [
-    "Generally you enjoy shooter games", "It's enjoyable to you to play horror games", "Your favorite games to play tend to fall in the MOBA category",
-    "Nothing excites you more than to grind away on an MMORPG", "You enjoy being the tank in team games", "You enjoy taking on the role of the healer", 
-    "You enjoy being the main source of Damage in a game, AKA the DPSer", "You like to hang back and play mages", "Do you tend to rage quit when a game is going badly?", 
-    "You blame your mistakes on lag, the game, and your teammates"
-]
 
+
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "app/public/home.html"));
+});
+
+app.get("/survey", function (req, res) {
+    res.sendFile(path.join(__dirname, "app/public/survey.html"));
+});
 
 app.listen(PORT, function (req,res) {
     console.log("You are connected on http://localhost:" + PORT);
